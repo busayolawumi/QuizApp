3839125e58757057b4dec84301750b24fb6c4f49
busayolawumi <busayoolawumi3@gmail.com>
2023-04-19 22:16:18 +0100

    More on UI after incorrect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Trivia from './Trivia'
 import { moneyList } from '../scripts/moneyList'
 import { data } from '../scripts/questions'
@@ -6,11 +6,15 @@ import { data } from '../scripts/questions'
 const Hero = () => { 
   const [questNumber, setQuestNumber] = useState(1)
   const [timer, setTimer] = useState(false)
-  const [earned, setEarned] = useState("$ 0")
+  const [earned, setEarned] = useState(`$ ${0}`)
+
+useEffect(() => {
+    questNumber > 1 && setEarned(moneyList.find((m) => m.questNum === questNumber - 1).questPrice);
+}, [moneyList, questNumber])
   return (
     <div className='app'>
       <div className="main">
-        {timer ? (<h1>You earned: {earned}</h1>) : (
+        {timer ? (<h1 className='endText'>You earned: {earned}</h1>) : (
       <>
         <div className="top">
           <div className="timer">30</div>
